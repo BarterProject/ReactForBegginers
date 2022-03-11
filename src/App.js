2cc0886732f33b87bdec57ad61747575a6df412d
melon0259 <melon0259@gmail.com>
2022-03-11 22:33:47 +0900

    useEffect로 컴포넌트 rerender 줄이기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+//import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+//const Memorized = memo();
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
+
+  useEffect(() => {
+    console.log("i run only one");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "") console.log("I run when keyword changes", keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when counter changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("둘중하나만바껴도 바뀜");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here..."
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
